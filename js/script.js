8092af6c31dd595f6e14cdc977353a049c72c1e4
syuhada-ramadhan <suhadahada18@gmail.com>
2021-01-01 18:08:43 +0700

    Project Mencetak Key dan Value di JavaScript selesai

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// objek mobil
+const mobil = {
+  warna: "putih",
+  tahunPembuatan: 2021,
+  bahanBakar: "bensin",
+  pajakHidup: true,
+};
+
+// for...in statement
+for (let key in mobil) console.log(key, mobil[key]);
